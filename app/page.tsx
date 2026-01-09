a5c7daa4a411d25fb16bcb05c2eb503d292f1dd1
void-or-Lloyd <tobias.bjorklund08@gmail.com>
2026-01-09 13:29:27 +0200

    done 2.0

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     console.log(data)
   }
 
-  getData()
+  getData() 
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
